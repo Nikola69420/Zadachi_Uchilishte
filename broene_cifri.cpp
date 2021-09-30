f203d393685e620aadb118d69fb137bc26fe6ede
Nikola69420 <82515628+Nikola69420@users.noreply.github.com>
2021-09-30 19:01:05 +0300

    Add files via upload

---
+++ broene_cifri.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    /// zad1
+    /*
+    int N=0;
+    cin>>N;
+    int fakeN = N, newNumber=0;
+    //int br=0;
+    int mini=N;
+    while(fakeN!=0)
+    {
+        int cifra=fakeN%10;
+        newNumber=newNumber*10 + cifra;
+        fakeN/=10;
+        if(mini>cifra)
+        {
+            mini=cifra;
+        }
+        //br++;
+    }
+    /// a podtochka
+    //cout<<br;
+    /// b podtochka
+    //cout<<newNumber+N;
+    /// w podtochka
+    int petcif=mini*10000+mini*1000+mini*100+mini*10+mini;
+    cout<<N-petcif;
+    */
+    ///zad2
+    int arr[10];
+    for(i=0;i<10;i++)
+    {
+        cin>>arr[i];
+    }
+    ///da dowursha
+
+    return 0;
+}
