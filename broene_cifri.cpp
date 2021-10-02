b9f87b46fa5215effbdda907d37ba52073bb807b
Nikola69420 <82515628+Nikola69420@users.noreply.github.com>
2021-10-02 18:24:20 +0300

    Update broene_cifri.cpp

--- broene_cifri.cpp
+++ broene_cifri.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cstdlib>
 using namespace std;
 int main ()
 {
@@ -25,16 +26,59 @@ int main ()
     /// b podtochka
     //cout<<newNumber+N;
     /// w podtochka
-    int petcif=mini*10000+mini*1000+mini*100+mini*10+mini;
-    cout<<N-petcif;
+    //int petcif=mini*10000+mini*1000+mini*100+mini*10+mini;
+    //cout<<abs(N-petcif);
     */
-    ///zad2
+    ///zad2 , da se napishe programa koqto razdelq masiw ot 10 chisla
+    ///na dwe chasti chiqto razlika e wuzmozhno nai malka
+
     int arr[10];
-    for(i=0;i<10;i++)
+    for(int i=0; i<10; i++)
     {
         cin>>arr[i];
     }
-    ///da dowursha
+    int num1, num2, extra=10000;
+    int mini=2147483646, index=0;
+    int m=0, n=0;
+    for(int i=0; i<5; i++)
+    {
+        index=i;
+        for(m=0; m<5; m++)
+        {
+            index%=10;
+            num1=arr[index]*extra;
+            extra/=10;
+            index++;
+        }
+        extra=10000;
+        for(n=0; n<5; n++)
+        {
+            index%=10;
+            num2=arr[index]*extra;
+            extra/=10;
+            index++;
 
+        }
+        if(mini>abs(num1-num2))
+        {
+            mini=abs(num1-num2);
+        }
+
+    }
+    cout<<endl<<mini;
     return 0;
 }
+
+/*for(i;i<i+5;i++)
+{
+
+
+
+}
+
+for(i%10;i<(i+5)%10;i++)
+{
+
+
+
+}*/
